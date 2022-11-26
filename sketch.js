025bc968cdb070737897e21544385300f27178d2
Florian Kittel <mail@florian-kittel.de>
2022-11-26 11:54:53 +0100

    feat(walker): add walker script

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let walker;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(60);
+  background(40);
 
   walker = new Walker(width / 2, height / 2);
 }
@@ -19,4 +19,12 @@ function windowResized() {
 function draw() {
   walker.show();
   walker.update();
+
+  // translate(width / 2, height / 2);
+  // let v = p5.Vector.random2D();
+  // v.mult(random(50, 100));
+
+  // stroke(255, 50);
+  // strokeWeight(2);
+  // line(0, 0, v.x, v.y);
 }
--- walker.js
+++ walker.js
@@ -1,15 +1,23 @@
 class Walker {
   pos;
+  from = -4;
+  to = 4;
+
   constructor(x, y) {
     this.pos = createVector(x, y);
+    // this.vel = p5.Vector.random2D();
+    // this.vel.mult(random(-1, 1));
   }
 
   update() {
-    this.pos.x = this.pos.x + random(-2, 2);
-    this.pos.y = this.pos.y + random(-2, 2);
+
+    // this.pos.add(this.vel);
+    this.pos.x = this.pos.x + random(this.from, this.to);
+    this.pos.y = this.pos.y + random(this.from, this.to);
   }
 
   show() {
+    // circle(this.pos.x, this.pos.y, 20);
     stroke(255, 100);
     strokeWeight(2);
     point(this.pos.x, this.pos.y);
