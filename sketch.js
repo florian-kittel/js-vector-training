7a426854a1825535bb55deeec450791051f993df
Florian Kittel <mail@florian-kittel.de>
2022-12-03 23:31:05 +0100

    feat(normalize): end of normalize tutorial

--- sketch.js
+++ sketch.js
@@ -18,20 +18,28 @@ function windowResized() {
 
 
 function draw() {
+  background(0);
+
   let pos = createVector(width / 2, height / 2);
   let mouse = createVector(mouseX, mouseY);
 
   let v = p5.Vector.sub(mouse, pos);
-  // v.normalize();
-  
-  // translate(width / 2, height / 2);
 
-  // stroke(255, 50);
-  // strokeWeight(2);
-  // line(0, 0, v.x, v.y);
+  // let m = v.mag();
+  // v.div(m);
+  v.normalize().mult(50);
+  // v.mult(50);
+  // console.log(m); 
+  // background(m);
+  // v.setMag(50);
 
-  walker.show();
-  walker.update();
+  translate(width / 2, height / 2);
+  stroke(200);
+  strokeWeight(4);
+  line(0, 0, v.x, v.y);
 
-  lines.update(); 
+  // walker.show();
+  // walker.update();
+
+  // lines.update(); 
 }
