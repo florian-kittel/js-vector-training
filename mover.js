00f828f72b4f5f54ce086baea6db6f05f4f99df7
Florian Kittel <mail@florian-kittel.de>
2022-12-03 23:50:12 +0100

    feat(vectors): end tutorial section vectors

---
+++ mover.js
@@ -0,0 +1,32 @@
+class Mover {
+  pos;
+  from = -4;
+  to = 4;
+  acc;
+
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(random(3));
+
+  }
+
+  update() {
+    let mouse = createVector(mouseX, mouseY);
+
+    this.acc = p5.Vector.sub(mouse, this.pos);
+    this.acc.setMag(0.1);
+    this.vel.add(this.acc);
+
+    this.vel.limit(5);
+
+    this.pos.add(this.vel);
+  }
+
+  show() {
+    stroke(255, 100);
+    strokeWeight(2);
+    fill(255, 100);
+    circle(this.pos.x, this.pos.y, 20);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -2,13 +2,14 @@ let pos;
 let x;
 let y;
 let walker;
+let mover;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(40);
 
-  walker = new Walker(width / 2, height / 2);
-  lines = new RandomLines(width / 2, height / 2);
+  mover = new Mover(width / 2, height / 2);
+  // lines = new RandomLines(width / 2, height / 2);
 }
 
 
@@ -18,28 +19,28 @@ function windowResized() {
 
 
 function draw() {
-  background(0);
+  background(40);
 
-  let pos = createVector(width / 2, height / 2);
-  let mouse = createVector(mouseX, mouseY);
+  // let pos = createVector(width / 2, height / 2);
+  // let mouse = createVector(mouseX, mouseY);
 
-  let v = p5.Vector.sub(mouse, pos);
+  // let v = p5.Vector.sub(mouse, pos);
 
   // let m = v.mag();
   // v.div(m);
-  v.normalize().mult(50);
+  // v.normalize().mult(50);
   // v.mult(50);
   // console.log(m); 
   // background(m);
   // v.setMag(50);
 
-  translate(width / 2, height / 2);
+  // translate(width / 2, height / 2); 
   stroke(200);
   strokeWeight(4);
-  line(0, 0, v.x, v.y);
+  // line(0, 0, v.x, v.y);
 
-  // walker.show();
-  // walker.update();
+  mover.show();
+  mover.update();
 
   // lines.update(); 
 }
