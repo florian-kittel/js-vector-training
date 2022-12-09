8e266abd8bb2d0a5f609aecacf1c034b6c4628fa
Florian Kittel <f.kittel@cross8.com>
2022-12-09 21:34:31 +0100

    feat(mover): add gravity and wind force

--- mover.js
+++ mover.js
@@ -2,20 +2,25 @@ class Mover {
   location;
   from = -4;
   to = 4;
-  acceleration;
+  acceleration = createVector(0, 0);
   velocity;
   topspeed = 10;
+  radius = 16;
 
   constructor(x, y) {
     this.location = createVector(x, y);
     this.velocity = p5.Vector.random2D();
-    this.velocity.mult(random(3));
+    this.velocity.mult(random(2));
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
   }
 
   update() {
     let mouse = createVector(mouseX, mouseY);
 
-    this.acceleration = p5.Vector.sub(mouse, this.location);
+    // this.acceleration = p5.Vector.sub(mouse, this.location);
     this.acceleration.setMag(0.1);
     this.velocity.add(this.acceleration);
 
@@ -28,20 +33,23 @@ class Mover {
     stroke(255, 100);
     strokeWeight(2);
     fill(255, 100);
-    circle(this.location.x, this.location.y, 20);
+    circle(this.location.x, this.location.y, this.radius * 2);
   }
 
   checkEdges() {
-    if (this.location.x > width) {
-      this.location.x = 0;
-    } else if (this.location.x < 0) {
-      this.location.x = width;
+    if (this.location.x > width - this.radius) {
+      this.location.x = width - this.radius;
+      this.velocity.x *= -1;
+    } else if (this.location.x < this.radius) {
+      this.location.x = this.radius;
+      this.velocity.x *= -1;
     }
 
-    if (this.location.y > height) {
-      this.location.y = 0;
-    } else if (this.location.y < 0) {
-      this.location.y = height;
+    if (this.location.y >= height - this.radius) {
+      this.location.y = height - this.radius;
+      this.velocity.y *= -1;
+    } else if (this.location.y < this.radius) {
+      this.location.y = this.radius;
     }
   }
 }
--- sketch.js
+++ sketch.js
@@ -35,9 +35,17 @@ function draw() {
   // v.setMag(50);
 
   // translate(width / 2, height / 2); 
-  stroke(200);
-  strokeWeight(4);
+  // stroke(200);
+  // strokeWeight(4);
   // line(0, 0, v.x, v.y);
+
+  if (mouseIsPressed) {
+    let wind = createVector(0.5, 0);
+    mover.applyForce(wind);
+  }
+
+  let gravity = createVector(0, 0.1);
+  mover.applyForce(gravity);
 
   mover.show();
   mover.update();
